26a0b53cecbc3402221546fe5d90766622c9c334
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-11-06 16:58:16 -0800

    Intro to array lists(finished), Guest Book(WIP)

--- src/_01_IntroToArrayLists/_02_GuestBook.java
+++ src/_01_IntroToArrayLists/_02_GuestBook.java
@@ -1,6 +1,13 @@
 package _01_IntroToArrayLists;
 
-public class _02_GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.nio.channels.ShutdownChannelGroupException;
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+public class _02_GuestBook implements ActionListener {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,6 +16,48 @@ public class _02_GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton add = new JButton("Add Name");
+	JButton view = new JButton("View Names");
+	ArrayList<String> list = new ArrayList<String>();
+	
+	
+	public static void main(String[] args) {
+		_02_GuestBook e = new _02_GuestBook();
+		e.GUI();
+	}
+	
+	void GUI() {
+	frame.add(panel);
+	panel.add(add);
+	panel.add(view);
+	
+	frame.setVisible(true);
+	add.addActionListener(this);
+	view.addActionListener(this);
+	list.add("Bob Banders");
+	list.add("Sandy Summers");
+	list.add("Greg Ganders");
+	list.add("Donny Doners");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		JButton ButtonPressed = (JButton) arg0.getSource();
+		
+		if(ButtonPressed == add) {
+			String name = JOptionPane.showInputDialog("Add a Name to the List");
+			list.add(name);
+		}
+		
+		if(ButtonPressed == view) {
+			
+		}
+	}
+	
+	
 	
 	
 	
