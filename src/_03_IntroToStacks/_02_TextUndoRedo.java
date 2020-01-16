be471a3c40d1e3c6eed9a467aaab02dbc5879781
jacobjessup2 <33267181+jacobjessup2@users.noreply.github.com>
2020-01-15 17:10:22 -0800

    Add files via upload

--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -1,18 +1,74 @@
 package _03_IntroToStacks;
 
-public class _02_TextUndoRedo {
-	/* 
-	 * Create a JFrame with a JPanel and a JLabel.
-	 * 
-	 * Every time a key is pressed, add that character to the JLabel. It should look like a basic text editor.
-	 * 
-	 * Make it so that every time the BACKSPACE key is pressed, the last character is erased from the JLabel.
-	 * Save that deleted character onto a Stack of Characters.
-	 * 
-	 * Choose a key to be the Undo key. Make it so that when that key is pressed, the top Character is popped 
-	 * off the Stack and added back to the JLabel.
-	 * 
-	 * */
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
+import javax.swing.*;
+
+public class _02_TextUndoRedo implements KeyListener {
 	
 	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	String labelString = "";
+	Stack<String> undo = new Stack<String>();
+	public static void main(String[] args) {
+		_02_TextUndoRedo e = new _02_TextUndoRedo();
+		e.Startup();
+	}
+
+	void Startup(){
+		frame.setVisible(true);
+		frame.setSize(500,50);
+		frame.add(panel);
+		panel.add(label);
+		frame.addKeyListener(this);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getKeyCode() == e.VK_BACK_SPACE) {
+			undo.push(labelString.substring(labelString.length()-1));
+			labelString = labelString.substring(0,labelString.length()-1);
+			System.out.println(undo.get(undo.size()-1));
+		}
+		
+		else if(e.getKeyCode() == e.VK_CONTROL){
+			System.out.println(undo.get(undo.size()-1));
+			labelString = labelString + undo.pop();
+		}
+		
+		else{
+			labelString = labelString + e.getKeyChar();
+		}
+		
+		label.setText(labelString);
+	}
+
+	private void elseif(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
+
+
 }
