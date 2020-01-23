8a718b7008623ad7e3a4cbde5a57634b6d4c9133
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2020-01-22 16:56:06 -0800

    Test Matching Brackets(Finished), Hangman (WIP)

--- src/_03_IntroToStacks/_03_TestMatchingBrackets.java
+++ src/_03_IntroToStacks/_03_TestMatchingBrackets.java
@@ -20,8 +20,33 @@ public class _03_TestMatchingBrackets {
 
 	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
 	private boolean doBracketsMatch(String b) {
-		
+		int brackValue = 0;	
+		Stack<String> test = new Stack<String>();
+		for (int i = 0; i < b.length(); i++) {
+			test.push(b.charAt(i)+"");
+		}
+		for (int i = 0; i < b.length(); i++) {
+			String yeet = test.pop();
+			System.out.println(yeet);
+			if(yeet.equals("}")) {
+				brackValue++;
+				System.out.println(brackValue);
+			}
+			if(yeet.equals("{")) {
+				brackValue--;
+				System.out.println(brackValue);
+				if(brackValue <= -1) {
+					return false;
+				}
+			}
+			
+		}
+		if(brackValue == 0) {
+			return true;
+		}
+		else {
 		return false;
+		}
 	}
 
 }
--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,42 @@
 package _04_HangMan;
 
-public class HangMan{
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.*;
+
+public class HangMan implements KeyListener{
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	String display = "";
+	String word = "";
 	
+	public static void main(String[] args) {
+		HangMan e = new HangMan();
+		e.setup();
+	}
+	void setup(){
+		frame.setVisible(true);
+		frame.addKeyListener(this);
+		frame.setSize(500,500);
+		frame.add(panel);
+		panel.add(label);
+	}
 	
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		
+	}
+	
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+				
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+				
+	}
 	
 }
