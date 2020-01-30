acc1dd27fe94cde8ba8f101b16db9fc418e956c7
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2020-01-29 16:58:44 -0800

    Hang Man (WIP)

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -2,15 +2,16 @@ package _04_HangMan;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Stack;
 
 import javax.swing.*;
-
 public class HangMan implements KeyListener{
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
 	String display = "";
 	String word = "";
+	Stack<String> words = new Stack<String>();
 	
 	public static void main(String[] args) {
 		HangMan e = new HangMan();
@@ -22,6 +23,15 @@ public class HangMan implements KeyListener{
 		frame.setSize(500,500);
 		frame.add(panel);
 		panel.add(label);
+	}
+	
+	void wordChoice() {
+		String leroy = JOptionPane.showInputDialog("give me a number between 1 and 266");
+		int jenkins = Integer.parseInt(leroy);
+		for (int i = 0; i < jenkins; i++) {
+			words.push(Utilities.readRandomLineFromFile("dictionary.txt"));
+		}
+		String display = words.pop();
 	}
 	
 	@Override
