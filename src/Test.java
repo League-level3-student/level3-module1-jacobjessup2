407bb4ea4fee2cccb61b0eb0628ab313e1938231
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2020-02-05 16:59:36 -0800

    Hangman(Finished)

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+
+public class Test {
+public static void main(String[] args) {
+	String e = "abcde";
+	
+	
+	System.out.println(e.substring(1,2));
+}
+}
--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -8,21 +8,30 @@ import javax.swing.*;
 public class HangMan implements KeyListener{
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-	JLabel label = new JLabel();
+	JLabel label1 = new JLabel();
+	JLabel label2 = new JLabel();
+	JLabel label3 = new JLabel();
 	String display = "";
 	String word = "";
+	String guessed = "";
+	int lives = 6;
 	Stack<String> words = new Stack<String>();
 	
 	public static void main(String[] args) {
 		HangMan e = new HangMan();
 		e.setup();
+		e.wordChoice();
+		e.shownWord();
 	}
 	void setup(){
 		frame.setVisible(true);
 		frame.addKeyListener(this);
-		frame.setSize(500,500);
+		frame.setSize(100,100);
 		frame.add(panel);
-		panel.add(label);
+		panel.add(label1);
+		panel.add(label2);
+		panel.add(label3);
+		
 	}
 	
 	void wordChoice() {
@@ -31,11 +40,53 @@ public class HangMan implements KeyListener{
 		for (int i = 0; i < jenkins; i++) {
 			words.push(Utilities.readRandomLineFromFile("dictionary.txt"));
 		}
-		String display = words.pop();
+		word = words.pop();
+	}
+	
+	void shownWord() {
+		for (int i = 0; i < word.length(); i++) {
+			if(word.substring(i,i+1) == " ") {
+				display = display + " ";
+			}
+			else {
+				display = display + "-";
+			}
+		}
+		label1.setText(display);
+		label2.setText("lives left: "+lives);
+		label3.setText("letters guessed: "+guessed);
 	}
 	
 	@Override
 	public void keyPressed(KeyEvent arg0) {
+		int check = 0;
+		guessed = guessed + arg0.getKeyChar();
+		label3.setText("letters guessed: "+guessed);
+		for (int i = 0; i < word.length(); i++) {
+			if(arg0.getKeyChar() == word.charAt(i)) {
+				check += 1;
+				display = display.substring(0,i) + word.charAt(i) + display.substring(i+1);
+			}
+		}
+
+		label1.setText(display);
+		
+		if(check == 0) {
+			lives--;
+			label2.setText("lives left: "+lives);
+		}
+		
+		if(lives == 0) {
+			JOptionPane.showMessageDialog(null, "You have RUN OUT of LIVES. The GAME will now CLOSE.");
+			System.exit(0);
+		}
+		
+		if(display.equals(word)) {
+			JOptionPane.showMessageDialog(null, "You COMPLETED the WORD! CREATING new WORD.");
+			display = "";
+			word = words.pop();
+			shownWord();
+		}
 		
 	}
 	
